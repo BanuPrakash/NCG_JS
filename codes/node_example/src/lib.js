2111a6cb88dace4a25009b5181260ced44a7dbe8
Banuprakash <banuprakashc@yahoo.co.in>
2025-07-02 11:45:32 +0530

    nodejs

---
+++ codes/node_example/src/lib.js
@@ -0,0 +1,29 @@
+
+function add(x, y) {
+    return x + y;
+}
+
+// using arrays forEach HOF function 
+function filter(elems, predicate) {
+    var result = [];
+    elems.forEach(function (elem) {
+        if(predicate(elem)) {
+            result.push(elem);
+        }
+    });
+    return result;
+}
+
+function map(elems, transform) {
+     var result = [];
+    elems.forEach(function (elem) {
+        result.push(transform(elem));
+    });
+    return result;
+}
+
+module.exports = {
+    add,
+    filter,
+    map
+}
