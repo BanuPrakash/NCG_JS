3612086239b732e5e490762d8c5338af99c5b706
Banuprakash <banuprakashc@yahoo.co.in>
2025-07-08 10:58:54 +0530

    cart Context

--- codes/productsapp/src/components/Cart.jsx
+++ codes/productsapp/src/components/Cart.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { CartContext } from './CartContextProviderComponent'
+import { Button, Container } from 'react-bootstrap';
+import CartRow from './CartRow'
 
 export default function Cart() {
+  let { cartItems, total } = useContext(CartContext);
   return (
-    <div>Cart</div>
+    <Container>
+      {
+        cartItems.map(product => <CartRow key={product.id} product={product} />)
+      }
+      <div className='row'>
+        <div className='col-md-10'>
+          &nbsp;
+        </div>
+        <div className='col-md-2'>
+          Total {total}
+        </div>
+      </div>
+      <div className='row'>
+        <div className='col-md-10'>
+          &nbsp;
+        </div>
+        <div className='col-md-2'>
+          <Button variant='success'>Checkout</Button>
+        </div>
+      </div>
+    </Container>
   )
 }
